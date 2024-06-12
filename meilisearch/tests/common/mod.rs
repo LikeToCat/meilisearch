369923831c916e0221483843cf26a814d8f99391
Tamo <tamo@meilisearch.com>
2024-06-12 18:27:03 +0200

    add tests on the rest embedder

--- meilisearch/tests/common/mod.rs
+++ meilisearch/tests/common/mod.rs
@@ -71,7 +71,14 @@ impl Display for Value {
         write!(
             f,
             "{}",
-            json_string!(self, { ".enqueuedAt" => "[date]", ".startedAt" => "[date]", ".finishedAt" => "[date]", ".duration" => "[duration]", ".processingTimeMs" => "[duration]" })
+            json_string!(self, {
+                ".enqueuedAt" => "[date]",
+                ".startedAt" => "[date]",
+                ".finishedAt" => "[date]",
+                ".duration" => "[duration]",
+                ".processingTimeMs" => "[duration]",
+                ".details.embedders.*.url" => "[url]"
+            })
         )
     }
 }
